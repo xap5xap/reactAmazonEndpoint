051df532efafe4ab9d2e1fb5256516e9a52007ed
Xavier Perez <xap5xap@users.noreply.github.com>
2017-10-25 11:25:57 -0500

    Add packages

--- App.js
+++ App.js
@@ -9,20 +9,38 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
+import { AsyncStorage } from 'react-native';
+
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
+  'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component<{}> {
+ async pone() {
+   console.log('pone')
+    await AsyncStorage.setItem('endpoint-arn', 'bla')
+  }
+
+  async registerWithSNS() {
+    let enpointArn = await this.retrieveEndPointArn();
+    console.log('enpointArn', enpointArn)
+  }
+
+  async retrieveEndPointArn() {
+    await AsyncStorage.getItem('endpoint-arn');
+  }
+
+  
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.container} >
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
@@ -32,6 +50,8 @@ export default class App extends Component<{}> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
+        <Button title="prueba" onPress={this.registerWithSNS.bind(this)} />
+        <Button title="pone" onPress={this.pone.bind(this)} />
       </View>
     );
   }
